fb0cc6e226daed97165947e26f6046a0c0aa6c87
akashmohanraj <akashabi7305@gmail.com>
2026-09-23 12:16:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
+import Navigation from "./Navigation";
 import React from "react";
-
-// TODO: Import the required components from react-router-dom
-// TODO: include the Home component
-// TODO: include the About Us component
-// TODO: include the Contact Us component
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 function App() {
-
   return (
-    <div>
+    <BrowserRouter>
+      <Navigation />
 
-      <header>
-        <h1>My React Website</h1>
-
-        <nav>
-          {/* TODO:
-              Create navigation links for:
-              1. Home
-              2. About Us
-              3. Contact Us
-          */}
-        </nav>
-      </header>
-
-
-      <main>
-
-        {/* TODO:
-            1. Add BrowserRouter
-            2. Add Routes
-            3. Create Route for Home
-            4. Create Route for About Us
-            5. Create Route for Contact Us
-        */}
-
-      </main>
-
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About us />} />
+        <Route path="/contact" element={<Contact us/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
